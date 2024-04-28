30e5a709ffd12e2abeb091a0bd37fb76bf4351ad
dave <dkl_hardwick@hotmail.com>
2024-04-28 18:51:44 +0100

    Plotting with missing anomaly data update (0.2.3)

--- data_collector.py
+++ data_collector.py
@@ -20,7 +20,7 @@ from ftplib import FTP
 from datetime import datetime, timezone
 
 DATE_TIME_FORMAT: str = "%Y-%m-%d %H:%M:%S.%f"
-VERSION: str = "0.2.2"
+VERSION: str = "0.2.3"
 
 
 class Status(Enum):
